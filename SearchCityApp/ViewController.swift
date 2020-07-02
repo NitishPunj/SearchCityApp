768be1b26fd0cc1fec72dd7558a500862457329c
NSharma <nathan@thebravelittletank.com>
2020-07-02 18:51:00 +0100

    Initial Commit

---
+++ SearchCityApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SearchCityApp
+//
+//  Created by N Sharma on 02/07/2020.
+//  Copyright © 2020 SharmaNitish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
